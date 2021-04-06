08321e4b6862c9c8df285ca1a12c704d45e6bb87
peterPalVanyo <vanyo.peter.pal@gmail.com>
2021-04-06 18:11:01 +0200

    add graph group inside the svg

--- src/main.js
+++ src/main.js
@@ -1,55 +1,67 @@
 import * as d3 from 'd3'
 import {attrs} from './selmulti'
 
-// DATA - önkormányzati lakások száma
-let data1 = [1424,462,3032,2382,1035,1034,2527,4375,2926,2536,1321,1128,5865,2571,1952,270,728,1338,648,663,1412,618,45]
-let cat = ['01ker', '02ker', '03ker', '04ker', '05ker', '06ker', '07ker', '08ker', '09ker', '10ker', '11ker', '12ker', '13ker', '14ker', '15ker', '16ker', '17ker', '18ker', '19ker', '20ker', '21ker', '22ker', '23ker']
-
-let districts = [{name:'01ker', onlak:1424, id:'elso'},{name:'02ker', onlak:462},{name:'03ker', onlak:3032},{name:'04ker', onlak:2382},{name:'05ker', onlak:1035},{name:'06ker', onlak:1034},{name:'07ker', onlak:2527},{name:'08ker', onlak:4375},{name:'09ker', onlak:2926},{name:'10ker', onlak:2536},{name:'11ker', onlak:1321},{name:'12ker', onlak:1128},{name:'13ker', onlak:5865},{name:'14ker', onlak:2571},{name:'15ker', onlak:1952},{name:'16ker', onlak:270},{name:'17ker', onlak:728},{name:'18ker', onlak:1338},{name:'19ker', onlak:648},{name:'20ker', onlak:663},{name:'21ker', onlak:1412},{name:'22ker', onlak:618},{name:'23ker', onlak:45},]
 
 /* IF U WANT TO SORT OUT */
 //districts.sort((a,b)=>a.onlak - b.onlak)
 
 let dim = {
-    'width': 800,
-    'height': 420
+    width: 600,
+    height: 300
 }
-
 let svg = d3.select('body').append('svg').attrs(dim).style('background', '#333')
+let margin = {top:20, bottom:50,left:40,right:10}
+const graphWidth = dim.width - margin.left - margin.right
+const graphHeight = dim.height - margin.top - margin.bottom
+const graph = svg.append('g').attrs({
+    width: graphWidth,
+    height: graphHeight,
+    transform: `translate(${margin.left}, ${margin.top})`
+})
+const xAxisGroup = svg.append('g').attrs({
+    color: 'white',
+    transform: `translate(${margin.left}, ${graphHeight+margin.top})`,
+    id: 'xaxis'
+})
+const yAxisGroup = svg.append('g').attrs({
+    color: 'white',
+    id: 'yaxis',
+    transform: `translate(${margin.left}, ${margin.top})`
+})
+
+
 
 d3.json('districts.json').then(districts => {
 
     //domain and range: top is the svg height-50
-    let scaleY = d3.scaleLinear([0, d3.max(districts, d=>d.onlak)], [350,50])
-    let scaleX = d3.scaleBand().domain(districts.map(d=>d.name)).rangeRound([50,780]).paddingOuter(0).paddingInner(.2)
+    let scaleY = d3.scaleLinear([0, d3.max(districts, d=>d.onlak)], [graphHeight,0])
+    let scaleX = d3.scaleBand().domain(districts.map(d=>d.name)).rangeRound([0,graphWidth]).paddingOuter(.3).paddingInner(.3)
     
     /* AXES - the axises, somehow the translateX didnot work */
-    
     let axisX = d3.axisBottom(scaleX)
     let axisY = d3.axisLeft(scaleY)
     let gridY = d3.axisLeft(scaleY)
     axisY.ticks(5)
     axisX.tickSize(0)
-    gridY.tickSize(-730).ticks(5).tickFormat('').tickSizeOuter(0)
-    //gridY
-    svg.append('g').attr('transform', 'translate(50,0)').call(gridY).selectAll('line').attrs({
+    gridY.tickSize(-1*graphWidth).ticks(5).tickFormat('').tickSizeOuter(0)
+    //gridY, if call to xAxisGroup: the later call diminish it
+    graph.append('g').call(gridY).selectAll('line').attrs({
         'stroke': 'white',
         'stroke-dasharray': '3,9'
     })
     //titleX
-    svg.append('g').attr('transform','translate(0,350)').attr('color','white').call(axisX).selectAll('text').attrs({
-        'font-size': 15,
-        'transform': 'rotate(-60), translate(-30,0)',
+    xAxisGroup.call(axisX)
+    d3.selectAll('#xaxis text').attrs({
+        'font-size': 14,
+        'text-anchor': 'end',
+        transform: `rotate(-90), translate(-8,-${scaleX.bandwidth()/2})`
     })
     //titleY
-    svg.append('g').style('font-size', '12px').attrs({
-        'transform': 'translate(50,0)',
-        'color': 'white',
-    }).call(axisY)
+    yAxisGroup.call(axisY)
     
     /* CHART - the content of the chart */
     
-    svg.selectAll('rect').data(districts).enter().append('rect').attrs({
+    graph.selectAll('rect').data(districts).enter().append('rect').attrs({
         'x': d => scaleX(d.name),
         'y': (d) => scaleY(d.onlak),
         'width': scaleX.bandwidth(),
@@ -58,7 +70,7 @@ d3.json('districts.json').then(districts => {
         'fill': '#808080',
         'stroke': 'white',
         'fill-opacity': .6,
-        'stroke-width': 2,
+        'stroke-width': 1.5,
     })
     
     svg.select('rect#elso').attr('fill', '#FFB0CD')
@@ -68,13 +80,13 @@ d3.json('districts.json').then(districts => {
     console.log(bars) */
     
     //circle at the top of the bars
-    svg.selectAll('circle').data(districts).enter().append('circle').attrs({
+    graph.selectAll('circle').data(districts).enter().append('circle').attrs({
         'cx': d => scaleX(d.name) + scaleX.bandwidth()/2,
         'cy': (d) => scaleY(d.onlak),
         'r': scaleX.bandwidth()/2,
         'fill': '#666',
         'stroke': 'white',
-        'stroke-width': 2,
+        'stroke-width': 1.5,
         id: (d) => d.id,
     })
     svg.select('circle#elso').attr('fill', '#FF7BAC')
