fea731e0f197771adce3ef4b59eae79acb02b958
peterPalVanyo <vanyo.peter.pal@gmail.com>
2021-04-15 11:41:07 +0200

    fixing: missed the loader for dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -13,5 +13,17 @@ module.exports = merge( common, {
   },
   plugins: [new HtmlWebpackPlugin({
     template: './src/template.html'
-  })]
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ]
+      }
+    ]
+  }
 });
